b7cf91c695b1b2d49a66bd91f73236fbe8746e43
Nate Ferrero <nate@grapheffect.com>
2012-09-10 17:09:21 -0700

    Undo debug

--- pyoauth2/provider.py
+++ pyoauth2/provider.py
@@ -413,10 +413,6 @@ class AuthorizationProvider(Provider):
         :rtype: requests.Response
         """
         params = utils.url_query_params(uri)
-
-
-
-        import ipdb; ipdb.set_trace()
         try:
             return self.get_authorization_code(**params)
         except TypeError as exc:
