72e00110137e08ed09b4b508cf9be1f2fb50b743
Nate Ferrero <nate@grapheffect.com>
2013-01-29 15:32:33 -0800

    Improve OAuth error logging ONLY, still return the same vague messages for security purposes

--- pyoauth2/provider.py
+++ pyoauth2/provider.py
@@ -444,8 +444,19 @@ class AuthorizationProvider(Provider):
         :rtype: requests.Response
         """
         try:
+            # Verify OAuth 2.0 Parameters
+            for x in ['grant_type', 'client_id', 'client_secret']:
+                if not data.get(x):
+                    raise TypeError("Missing required OAuth 2.0 POST param: {}".format(x))
+            
+            # Handle get token from refresh_token
             if 'refresh_token' in data:
                 return self.refresh_token(**data)
+
+            # Handle get token from authorization code
+            for x in ['redirect_uri', 'code']:
+                if not data.get(x):
+                    raise TypeError("Missing required OAuth 2.0 POST param: {}".format(x))            
             return self.get_token(**data)
         except TypeError as exc:
             self._handle_exception(exc)
